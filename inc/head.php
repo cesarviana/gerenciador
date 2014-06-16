b6b041fe37b5f1ada914f6ca17d31f62b8d1054c
Cesar Viana <cesar.pviana@gmail.com>
2014-06-16 16:38:39 -0300

    Adicionado frameworks

---
+++ inc/head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="ISO-8859-1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Definir o valor da variável $sTitulo antes de incluir este arquivo -->
+	<title> $sTitulo </title>
+	<link rel="stylesheet" href="<?php echo URL ?>/frameworks/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo URL ?>/frameworks/bootstrap/css/bootstrap-theme.min.css">
+	<script src="<?php echo URL ?>/frameworks/bootstrap/css/bootstrap.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
